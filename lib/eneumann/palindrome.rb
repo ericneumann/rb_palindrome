2445ece3f0d2184e788f16f0b47e5699469c0cf8
Eric Neumann <ericn@hotmail.com>
2021-02-08 16:00:38 +0800

    refactored palindrome

--- lib/eneumann/palindrome.rb
+++ lib/eneumann/palindrome.rb
@@ -2,9 +2,26 @@
 
 require_relative "palindrome/version"
 
-module Eneumann
-  module Palindrome
-    class Error < StandardError; end
-    # Your code goes here...
+
+
+# module Eneumann
+#   module Palindrome
+#     class Error < StandardError; end
+#     # Your code goes here...
+#   end
+# end
+
+class String
+
+  # Returns true for a palindrome, false otherwise.
+  def palindrome?
+    processed_content == processed_content.reverse
   end
+
+  private
+
+    # Returns content for palindrome testing.
+    def processed_content
+      self.scan(/[a-zA-Z+]/).join.downcase
+    end
 end
